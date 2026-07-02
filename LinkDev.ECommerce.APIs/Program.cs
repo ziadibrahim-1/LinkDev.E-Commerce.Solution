82a141ebfb7cc8ae4b67ae07801c4c3d6b1cda64
Ziadali12 <ziadali2341@gmail.com>
2026-07-02 16:12:46 +0300

    Add Update-database with code

--- LinkDev.ECommerce.APIs/Program.cs
+++ LinkDev.ECommerce.APIs/Program.cs
@@ -1,9 +1,12 @@
+using LinkDev.ECommerce.Infrastructure.Peresistence.Data;
 using LinkDev.ECommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 namespace LinkDev.ECommerce.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +25,32 @@ namespace LinkDev.ECommerce.APIs
 
             var app = builder.Build();
 
+            #region Update Database
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<StoreContext>();
+
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+                if(pendingMigrations.Any())
+                     await context.Database.MigrateAsync();
+                
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+            #endregion
+
             #region Configure
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-               
+
                 app.MapOpenApi();
                 app.UseSwaggerUI();
                 app.UseSwagger();
@@ -37,7 +61,7 @@ namespace LinkDev.ECommerce.APIs
             app.UseAuthorization();
 
 
-            app.MapControllers(); 
+            app.MapControllers();
             #endregion
 
             app.Run();
