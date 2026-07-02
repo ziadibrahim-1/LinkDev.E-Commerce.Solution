9455638e1e19e70dab9de2a5f4dd71282590bf93
Ziadali12 <ziadali2341@gmail.com>
2026-07-02 04:12:31 +0300

    Create Common Entities

---
+++ LinkDev.ECommerce.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.ECommerce.Domain.Common
+{
+    public abstract class BaseAuditableEntity<TKey> : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public required string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
---
+++ LinkDev.ECommerce.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.ECommerce.Domain.Common
+{
+    public abstract class BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public required TKey Id { get; set; }
+    }
+}
